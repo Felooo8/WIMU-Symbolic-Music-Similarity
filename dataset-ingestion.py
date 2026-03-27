ebcdcb83707e836ee548a5c40adf77e8d901cc72
Mateusz <mateusz.baran3008@onet.pl>
2026-03-27 23:41:17 +0100

    code refactor to template method design pattern

--- dataset-ingestion.py
+++ dataset-ingestion.py
@@ -2,7 +2,12 @@ import yaml
 import muspy
 import wandb
 import os
+import logging
 from dotenv import load_dotenv
+
+from data_providers import DatasetFactory
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 
 def load_config(config_path="configs/config.yaml"):
@@ -10,69 +15,22 @@ def load_config(config_path="configs/config.yaml"):
         return yaml.safe_load(f)
 
 
-mapping = {
-    "maestro_v3": muspy.MAESTRODatasetV3,
-    "lakh_midi": muspy.LakhMIDIAlignedDataset,
-    "jsb_chorales": muspy.JSBChoralesDataset,
-    "nes_mdb": muspy.NESMusicDatabase
-}
-
-
-def get_muspy_dataset_class(dataset_name):
-    if dataset_name not in mapping:
-        raise ValueError(f"Unsupported dataset: {dataset_name}. Available options are: {list(mapping.keys())}")
-
-    return mapping[dataset_name]
-
-
-def load_and_parse_dataset(dataset_cfg):
-    path = dataset_cfg["path"]
-    name = dataset_cfg["name"]
-    sample_size = dataset_cfg.get("sample_size", float('inf'))
-
-    print(f"\n[{name}] Starting processing (Limit: {sample_size} files)...")
-
-    DatasetClass = get_muspy_dataset_class(name)
-    if name == "lakh_midi":
-        dataset = DatasetClass(path, download_and_extract=True)
-    else:
-        dataset = DatasetClass(path, download_and_extract=False)
-
-    valid_scores = []
-    errors = 0
-
-    for i, score in enumerate(dataset):
-        if i >= sample_size:
-            print(f"[{name}] Sample limit reached ({sample_size}). Stopping ingestion.")
-            break
-
-        if score is not None and len(score.tracks) > 0:
-            valid_scores.append(score)
-        else:
-            errors += 1
-
-    print(f"[{name}] Finished. Valid: {len(valid_scores)}, Errors: {errors}")
-    return valid_scores, errors
-
-
 def main():
     cfg = load_config()
     load_dotenv()
-    api_key = os.getenv("WANDB_API_KEY")
 
-    if api_key:
-        wandb.login(key=api_key)
-    else:
+    api_key = os.getenv("WANDB_API_KEY")
+    if not api_key:
         raise ValueError("Missing WANDB_API_KEY or .env file!")
 
-    run = wandb.init(
+    wandb.login(key=api_key)
+
+    wandb.init(
         project=cfg["wandb"]["project"],
         entity=cfg["wandb"]["entity"],
         config=cfg,
         name="full-dataset-ingestion"
     )
-
-    all_datasets_data = {}
 
     dataset_artifact = wandb.Artifact(
         name="sampled-symbolic-datasets",
@@ -84,8 +42,9 @@ def main():
     os.makedirs(processed_dir, exist_ok=True)
 
     for dataset_key, dataset_cfg in cfg["datasets"].items():
-        parsed_scores, error_count = load_and_parse_dataset(dataset_cfg)
-        all_datasets_data[dataset_key] = parsed_scores
+        provider = DatasetFactory.create(dataset_cfg)
+
+        parsed_scores, error_count = provider.process()
 
         wandb.log({
             f"{dataset_key}/successfully_parsed": len(parsed_scores),
@@ -98,14 +57,12 @@ def main():
         for i, score in enumerate(parsed_scores):
             out_path = os.path.join(dataset_out_dir, f"score_{i:04d}.json")
             muspy.save(out_path, score)
-
             dataset_artifact.add_file(out_path, name=f"{dataset_key}/score_{i:04d}.json")
 
-    print("\nSending W&B artifact")
-    run.log_artifact(dataset_artifact)
-
+    logging.info("Sending W&B artifact...")
+    wandb.log_artifact(dataset_artifact)
     wandb.finish()
-    print("Run W&B done. Check the artifact on the W&B!")
+    logging.info("Run W&B done. Check the artifact on the W&B!")
 
 
 if __name__ == "__main__":
